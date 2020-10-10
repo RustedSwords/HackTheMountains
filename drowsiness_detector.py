87d2de7be3d4a122730063d544d3e6762a8555a1
Manav Makwana <49590817+RustedSwords@users.noreply.github.com>
2020-10-10 16:12:07 +0530

    Update drowsiness_detector.py
    defined some major functions

--- drowsiness_detector.py
+++ drowsiness_detector.py
@@ -10,3 +10,49 @@ from imutils.video import VideoStream
 from imutils import face_utils
 from threading import Thread
 from threading import Timer
+
+def eye_aspect_ratio(eye) :
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+    
+def init_open_ear() :
+    time.sleep(5)
+    print("open init time sleep")
+    ear_list = []
+    th_message1 = Thread(target = init_message)
+    th_message1.deamon = True
+    th_message1.start()
+    for i in range(7) :
+        ear_list.append(both_ear)
+        time.sleep(1)
+    global OPEN_EAR
+    OPEN_EAR = sum(ear_list) / len(ear_list)
+    #print("open list =", ear_list)
+    print("OPEN_EAR =", OPEN_EAR, "\n")
+
+def init_close_ear() : 
+    time.sleep(2)
+    th_open.join()
+    time.sleep(5)
+    print("close init time sleep")
+    ear_list = []
+    th_message2 = Thread(target = init_message)
+    th_message2.deamon = True
+    th_message2.start()
+    time.sleep(1)
+    for i in range(7) :
+        ear_list.append(both_ear)
+        time.sleep(1)
+    CLOSE_EAR = sum(ear_list) / len(ear_list)
+    global EAR_THRESH
+    EAR_THRESH = (((OPEN_EAR - CLOSE_EAR) / 2) + CLOSE_EAR) #EAR_THRESH means 50% of the being opened eyes state
+    #print("close list =", ear_list)
+    print("CLOSE_EAR =", CLOSE_EAR, "\n")
+    print("The last EAR_THRESH's value :",EAR_THRESH, "\n")
+
+def init_message() :
+    print("init_message")
+    alarm.sound_alarm("init_sound.mp3")
